88a0e3133cb4346e81cc6a3cf392ce9a7c810fcd
Heiko Braun <ike.braun@googlemail.com>
2013-04-08 18:06:37 +0200

    heap example updates

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,21 +5,32 @@
 
 function JvmCtrl($scope, Dispatcher) {
 
+    $scope.heap = new dmr.ModelNode();
+
+    // /host=master/core-service=platform-mbean/type=memory
+
     $scope.load = function() {
         var op = new dmr.ModelNode();
-        op.get("operation").set("read-attribute");
-        op.get("address").setEmptyList();
-        op.get("name").set("release-version");
+        op.get("operation").set("read-resource");
+        op.get("address").addProp("host", "master");
+        op.get("address").addProp("core-service", "platform-mbean");
+        op.get("address").addProp("type", "memory");
+        op.get("include-runtime").setBoolean(true);
 
         Dispatcher.execute(op, function(err, results){
 
             if(err!=null)
             {
-                alert("Operation failed");
+                alert("Operation failed.");
             }
             else
             {
-                alert(results);
+                //alert(results);
+                console.log(results.toString());
+
+                var response = results.get("result");
+                $scope.heap = response.get("heap-memory-usage");
+                $scope.$apply();
             }
         });
 
